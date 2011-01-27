0da81e85e9119a7ef570245a93ea72775f3141ee
Tom Kidd <tomkidd@gmail.com>
2011-01-26 21:08:13 -0600

    Initial version

---
+++ SteamShortcutEditor/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace SteamShortcutEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtGamesDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSteamDir = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(85, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(4, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtGamesDir
+            // 
+            this.txtGamesDir.Location = new System.Drawing.Point(63, 12);
+            this.txtGamesDir.Name = "txtGamesDir";
+            this.txtGamesDir.Size = new System.Drawing.Size(697, 20);
+            this.txtGamesDir.TabIndex = 3;
+            this.txtGamesDir.Text = "D:\\games";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Games Dir:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(766, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(26, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(766, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(26, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "...";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Steam Dir:";
+            // 
+            // txtSteamDir
+            // 
+            this.txtSteamDir.Location = new System.Drawing.Point(63, 38);
+            this.txtSteamDir.Name = "txtSteamDir";
+            this.txtSteamDir.Size = new System.Drawing.Size(697, 20);
+            this.txtSteamDir.TabIndex = 6;
+            this.txtSteamDir.Text = "D:\\games\\steam";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(8, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(784, 316);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(554, 392);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Scan";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(635, 392);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Export";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Location = new System.Drawing.Point(717, 392);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Load";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button8.Location = new System.Drawing.Point(167, 390);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 13;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 425);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSteamDir);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGamesDir);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtGamesDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSteamDir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ SteamShortcutEditor/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace SteamShortcutEditor
+{
+    public partial class Form1 : Form
+    {
+        List<Game> games;
+        DataTable dtGames;
+
+        public Form1()
+        {
+            DataColumn dcAppName = new DataColumn("AppName");
+            DataColumn dcExe = new DataColumn("Exe");
+            DataColumn dcStartDir = new DataColumn("StartDir");
+
+            dtGames = new DataTable();
+            dtGames.Columns.Add(dcAppName);
+            dtGames.Columns.Add(dcExe);
+            dtGames.Columns.Add(dcStartDir);
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filename = @"D:\games\steam\config\shortcuts - Copy.vdf";
+            StringBuilder sb = GetByteText(filename);
+
+           // textBox1.Text = sb.ToString();
+        }
+
+        private static StringBuilder GetByteText(string filename)
+        {
+            byte[] buff = null;
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fs);
+            long numBytes = new FileInfo(filename).Length;
+            buff = br.ReadBytes((int)numBytes);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in buff)
+            {
+                //char c = '\0';
+                //try{c = (char)b;}catch{}
+                if ((int)b != 0)
+                {
+                    sb.AppendLine(string.Format("\"{3}\"\t{0}\tU+{1:x4}\t{2}", b, (int)b, (int)b, (char)b));
+                }
+                else
+                {
+                    sb.AppendLine(string.Format("\"{3}\"\t{0}\tU+{1:x4}\t{2}", b, (int)b, (int)b, ""));
+                }
+            }
+            return sb;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte byteZero = byte.Parse("0");
+            byte byteOne = byte.Parse("1");
+            byte byteEight = byte.Parse("8");
+            byte[] byteShortcuts = Encoding.ASCII.GetBytes("Shortcuts");
+            byte[] byteAppName = Encoding.ASCII.GetBytes("AppName");
+            byte[] byteExe = Encoding.ASCII.GetBytes("Exe");
+            byte[] byteStartDir = Encoding.ASCII.GetBytes("StartDir");
+            byte[] byteIcon = Encoding.ASCII.GetBytes("icon");
+            byte[] byteTags = Encoding.ASCII.GetBytes("tags");
+
+            List<byte> lbytes = new List<byte>();
+
+            lbytes.Add(byteZero);
+            //lbytes.Add(byteOne);
+            lbytes.AddRange(byteShortcuts);
+            lbytes.Add(byteZero);
+
+            List<Game> games = new List<Game>();
+
+            foreach (DataRow drGame in dtGames.Rows)
+            {
+                Game game = new Game(drGame["AppName"].ToString() + " (Non-Steam)", drGame["Exe"].ToString(), drGame["StartDir"].ToString());
+                games.Add(game);
+            }
+
+            int i = 0;
+
+            foreach (Game game in games)
+            {
+                lbytes.Add(byteZero);
+
+                byte[] byteIndex = Encoding.ASCII.GetBytes(i.ToString());
+                lbytes.AddRange(byteIndex);
+
+                lbytes.Add(byteZero);
+                lbytes.Add(byteOne);
+
+                lbytes.AddRange(byteAppName);
+                lbytes.Add(byteZero);
+                lbytes.AddRange(Encoding.ASCII.GetBytes(game.AppName));
+                lbytes.Add(byteZero);
+                lbytes.Add(byteOne);
+
+                lbytes.AddRange(byteExe);
+                lbytes.Add(byteZero);
+                lbytes.AddRange(Encoding.ASCII.GetBytes(game.Exe));
+                lbytes.Add(byteZero);
+                lbytes.Add(byteOne);
+
+                lbytes.AddRange(byteStartDir);
+                lbytes.Add(byteZero);
+                lbytes.AddRange(Encoding.ASCII.GetBytes(game.StartDir));
+                lbytes.Add(byteZero);
+                lbytes.Add(byteOne);
+
+                lbytes.AddRange(byteIcon);
+                lbytes.Add(byteZero);
+                lbytes.Add(byteZero);
+                lbytes.Add(byteZero);
+
+                lbytes.AddRange(byteTags);
+                lbytes.Add(byteZero);
+                lbytes.Add(byteEight);
+                lbytes.Add(byteEight);
+
+                i++;
+            }
+
+            lbytes.Add(byteEight);
+            lbytes.Add(byteEight);
+            
+            byte[] buffer = lbytes.ToArray();
+
+            string outputFilename = txtSteamDir.Text + "\\config\\shortcuts.vdf";
+
+            if (File.Exists(outputFilename))
+            {
+                File.Delete(outputFilename);
+            }
+
+            FileStream fs = new FileStream(outputFilename, FileMode.Create, FileAccess.ReadWrite);
+            BinaryWriter bw = new BinaryWriter(fs);
+            bw.Write(buffer);
+            bw.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            games = new List<Game>();
+
+            GameSearch(txtGamesDir.Text);
+
+            foreach (Game game in games)
+            {
+                DataRow drGame = dtGames.NewRow();
+                drGame["AppName"] = game.AppName;
+                drGame["Exe"] = game.Exe;
+                drGame["StartDir"] = game.StartDir;
+                dtGames.Rows.Add(drGame);
+            }
+
+            dataGridView1.DataSource = dtGames;
+        }
+
+        private void GameSearch(string sDir)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(sDir);
+                foreach (DirectoryInfo subdi in di.GetDirectories())
+                {
+                    foreach (FileInfo fi in subdi.GetFiles("*.exe"))
+                    {
+                        if (!fi.FullName.Contains(txtSteamDir.Text))
+                        {
+                            games.Add(new Game(fi.Name.Replace(fi.Extension,""), fi.FullName, fi.Directory.FullName));
+                        }
+                    }
+                    GameSearch(subdi.FullName);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlGames = new XmlDocument();
+            XmlElement xeGames = xmlGames.CreateElement("games");
+
+            foreach (DataRow dr in dtGames.Rows)
+            {
+                //games.Add(new Game(dr["AppName"].ToString(), dr["Exe"].ToString(), dr["StartDir"].ToString()));
+                XmlElement xeGame = xmlGames.CreateElement("game");
+                XmlElement xeAppName = xmlGames.CreateElement("AppName");
+                XmlElement xeExe = xmlGames.CreateElement("Exe");
+                XmlElement xeStartDir = xmlGames.CreateElement("StartDir");
+
+                xeAppName.InnerText = dr["AppName"].ToString();
+                xeExe.InnerText = dr["Exe"].ToString();
+                xeStartDir.InnerText = dr["StartDir"].ToString();
+
+                xeGame.AppendChild(xeAppName);
+                xeGame.AppendChild(xeExe);
+                xeGame.AppendChild(xeStartDir);
+
+                xeGames.AppendChild(xeGame);
+            }
+
+            xmlGames.AppendChild(xeGames);
+
+            if (File.Exists("games.xml"))
+            {
+                File.Delete("games.xml");
+            }
+
+            xmlGames.Save("games.xml");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlGames = new XmlDocument();
+            xmlGames.Load("games.xml");
+
+            XmlNodeList xnlGames = xmlGames.GetElementsByTagName("game");
+            
+            dtGames.Clear();
+
+            foreach (XmlElement xeGame in xnlGames)
+            {
+                DataRow drGame = dtGames.NewRow();
+                drGame["AppName"] = xeGame["AppName"].InnerText;
+                drGame["Exe"] = xeGame["Exe"].InnerText;
+                drGame["StartDir"] = xeGame["StartDir"].InnerText;
+                dtGames.Rows.Add(drGame);
+            }
+
+            dataGridView1.DataSource = dtGames;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dtGames.Rows.Count.ToString());
+        }
+    }
+
+    public class Game
+    {
+        public Game()
+        {
+        }
+
+        public Game(string AppName, string Exe, string StartDir)
+        {
+            this.AppName = AppName;
+            this.Exe = Exe;
+            this.StartDir = StartDir;
+        }
+
+        private string appName;
+
+        public string AppName
+        {
+            get { return appName; }
+            set { appName = value; }
+        }
+
+        private string exe;
+
+        public string Exe
+        {
+            get { return exe; }
+            set { exe = value; }
+        }
+
+        private string startDir;
+
+        public string StartDir
+        {
+            get { return startDir; }
+            set { startDir = value; }
+        }
+    }
+
+}
